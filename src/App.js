edeec16dc1394165464a1dee8b00bf5bb26bf8c0
nnjustin44 <nnjustin44@gmail.com>
2020-10-06 11:29:12 -0400

    First Push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <title>Intermitant Fasting</title>
+      <header className="App-header">Intermitant Fasting</header>
+      <body className="body">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="timer">
+          <code>Timer</code>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reactjs.org" //Change link to databse record
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go To Record
         </a>
-      </header>
+      </body>
     </div>
   );
 }
